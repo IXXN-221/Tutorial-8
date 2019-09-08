a4fdfc529429ccf512224058c5386d8fa702ab29
EmilSchroder <eschro66@gmail.com>
2019-09-09 11:02:57 +1200

    initial commit

---
+++ instruction.md
@@ -0,0 +1,13 @@
+Create a page that lists out a bunch of movie data for the viewer to enjoy.
+
+Review the wireframe, and data in the main.js file.
+
+1. Create a function called printMovie() in main.js
+2. Create a variable, then assign it to a new array created by using the 'map' method on the movieData.  The map should return a template string with the relevant HTML.
+3. Use the 'join' method to turn the variable into one string and set it as the innerHTML of the 'mainContent' div.
+
+Stretch
+4. Add in the average of all the review scores. 
+
+Further Stretch
+5. Try some other array methods out at https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-javascript/manipulate-arrays-with-push
---
+++ main.js
@@ -0,0 +1,32 @@
+let movieData = [
+    {
+        title: 'The Hunt' ,
+        releaseDate: 2012,
+        reviewScores: [8,8,9,8,9,8,7,7,8,7,8,9,8],
+        summary: "A teacher lives a lonely life, all the while struggling over his son's custody. His life slowly gets better as he finds love and receives good news from his son, but his new luck is about to be brutally shattered by an innocent little lie.",
+        image: './images/Dogcar.jpg'
+    },
+    {
+        title:'How to Train Your Dragon',
+        releaseDate: 2010,
+        reviewScores:[8,7,7,8,9,5,6,4,8,7,9,9],
+        summary: "A hapless young Viking who aspires to hunt dragons becomes the unlikely friend of a young dragon himself, and learns there may be more to the creatures than he assumed.",
+        image: './images/Doghead.jpg'
+    },
+    {
+        title: 'Like Stars on Earth',
+        releaseDate: 2007,
+        reviewScores:[6,9,5,4,7,5,4,7,8,8,9,10,2,5,8],
+        summary: "An eight-year-old boy is thought to be a lazy trouble-maker, until the new art teacher has the patience and compassion to discover the real problem behind his struggles in school.",
+        image: './images/Douge.jpg'
+    },
+    {
+        title: "Before Sunset",
+        releaseDate: 2004,
+        reviewScores: [9,8,8,8,7,5,8,8,9,8],
+        summary: "Nine years after Jesse and Celine first met, they encounter each other again on the French leg of Jesse's book tour.",
+        image: './images/WinkDog.jpg' 
+    }
+]
+
+
